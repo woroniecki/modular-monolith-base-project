596ea17d15d1316cfa9d2f7ff1bea3301247315a
woroniecki <dwrona94@gmail.com>
2025-03-26 21:25:29 +0100

    Changed swagger generator

--- Frontend/frontend-spa-app/src/app/api-client/api/api.ts
+++
@@ -1,5 +0,0 @@
-export * from './account.service';
-import { AccountService } from './account.service';
-export * from './healthCheck.service';
-import { HealthCheckService } from './healthCheck.service';
-export const APIS = [AccountService, HealthCheckService];
