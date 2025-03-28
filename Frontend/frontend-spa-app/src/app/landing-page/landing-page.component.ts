37d8cbac9f7509d5170d9fd538de7d41eae40098
woroniecki <dwrona94@gmail.com>
2025-03-28 16:00:10 +0100

    Some page when logged in

--- Frontend/frontend-spa-app/src/app/landing-page/landing-page.component.ts
+++ Frontend/frontend-spa-app/src/app/landing-page/landing-page.component.ts
@@ -2,18 +2,23 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrl: './landing-page.component.css',
 })
 export class LandingPageComponent {
   title = 'frontend-spa-app';
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    public auth: AuthService,
+  ) {}
 
   onRegister() {
     this.router.navigate(['/register']);
